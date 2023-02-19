655e28a86b4cf75b6f9a39dbd0aebd9c24d2a6c5
Manon Sanoi <info@manonsanoi.com>
2023-02-20 06:12:45 +0700

    Create database.sh

---
+++ database.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+case "$1" in
+  add)
+    read -p "Enter database name: " dbname
+    read -p "Enter database admin username: " dbuser
+    read -s -p "Enter database admin password: " dbpass
+
+    mysql -u root -e "CREATE DATABASE \\\`$dbname\\\`;"
+    mysql -u root -e "GRANT ALL PRIVILEGES ON \\\`$dbname\\\`.* TO '$dbuser'@'localhost' IDENTIFIED BY '$dbpass';"
+    mysql -u root -e "FLUSH PRIVILEGES;"
+
+    read -p "Would you like to add a user to the database? (y/n): " adduser
+    if [[ \$adduser == "y" ]]; then
+      read -p "Enter username: " username
+      read -s -p "Enter password: " userpass
+      mysql -u root -e "GRANT ALL PRIVILEGES ON \\\`$dbname\\\`.* TO '$username'@'localhost' IDENTIFIED BY '$userpass';"
+      mysql -u root -e "FLUSH PRIVILEGES;"
+      echo "User \\\`\$username\\\` added to database \\\`\$dbname\\\` with password \\\`\$userpass\\\`."
+    fi
+    ;;
+  delete)
+    read -p "Enter database name to delete: " dbname
+    mysql -u root -e "DROP DATABASE \\\`$dbname\\\`;"
+    echo "Database \\\`\$dbname\\\` deleted."
+    ;;
+  *)
+    echo "Usage: $0 {add|delete}"
+    exit 1
+    ;;
+esac
