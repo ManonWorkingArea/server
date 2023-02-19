e44d559a31769f172d68e7dff4e93bbe67ab59d9
Manon Sanoi <info@manonsanoi.com>
2023-02-20 06:27:29 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,23 +15,6 @@ timedatectl set-timezone Asia/Bangkok
 apt-get update
 apt-get upgrade -y
 
-# Optimize system for best performance
-echo "vm.swappiness=10" >> /etc/sysctl.conf
-echo "vm.vfs_cache_pressure=50" >> /etc/sysctl.conf
-echo "net.core.somaxconn=65535" >> /etc/sysctl.conf
-echo "fs.file-max=2097152" >> /etc/sysctl.conf
-echo "kernel.pid_max=4194304" >> /etc/sysctl.conf
-echo "kernel.sysrq=1" >> /etc/sysctl.conf
-echo "fs.nr_open=1048576" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_syncookies=1" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_max_syn_backlog=8192" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_fin_timeout=10" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_keepalive_time=300" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_tw_reuse=1" >> /etc/sysctl.conf
-echo "net.ipv4.ip_local_port_range=1024 65535" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_tw_recycle=1" >> /etc/sysctl.conf
-sysctl -p
-
 # Configure Mariadb for remote access
 sed -i 's/bind-address.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 systemctl restart mariadb.service
@@ -43,6 +26,14 @@ ufw allow 3306/tcp
 
 # Enable firewall
 ufw --force enable
+
+# Download and make executable database.sh
+curl -o database.sh https://raw.githubusercontent.com/ManonWorkingArea/server/main/database.sh
+chmod +x database.sh
+
+# Download and make executable backup.sh
+curl -o backup.sh https://raw.githubusercontent.com/ManonWorkingArea/server/main/backup.sh
+chmod +x backup.sh
 
 # Add alias for backup
 echo "alias backup='/root/backup.sh'" >> ~/.bashrc
